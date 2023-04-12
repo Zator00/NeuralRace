bb5f6d902a3c111c28cea9ed1b78010a05a0ea32
MrDawkin <mr.dawkin@wp.pl>
2023-04-12 21:48:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import pyglet
 import os
 
@@ -7,17 +8,30 @@ height = 720
 window = pyglet.window.Window(style=pyglet.window.Window.WINDOW_STYLE_DEFAULT, caption='Neural Race')
 window.set_size(width, height)
 
-label = pyglet.text.Label('Start Neural Race',
+start = pyglet.text.Label('Start Neural Race',
                           font_name ='Times New Roman',
                           font_size = 24,
-                          x = 10, y = 10,
-                          anchor_x ='left', anchor_y ='bottom')
+                          x = width/2, y = height/2 + 80,
+                          anchor_x ='center', anchor_y ='center')
+
+settings = pyglet.text.Label('Settings',
+                          font_name ='Times New Roman',
+                          font_size = 24,
+                          x = width/2, y = height/2,
+                          anchor_x ='center', anchor_y ='center')
+
+exitText = pyglet.text.Label('Exit game',
+                          font_name ='Times New Roman',
+                          font_size = 24,
+                          x = width/2, y = height/2 - 80,
+                          anchor_x ='center', anchor_y ='center')
 
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
+    start.draw()
+    settings.draw()
+    exitText.draw()
+
 
 pyglet.app.run()
-
-print("Hello")
